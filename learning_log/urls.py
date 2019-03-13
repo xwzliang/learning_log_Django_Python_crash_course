8e6c94825468b5e5ea99ecb838717cc73e56a5e5
Broliang <18706703437@163.com>
2019-03-13 09:39:30 +0000

    Allow user to login

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
+from django.conf.urls import include as conf_include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
+    path('users/', include('users.urls')),
+    path(r'', conf_include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
 ]
