301b5df23e28e4d91b689c670fceb476f9be82b8
Broliang <18706703437@163.com>
2019-03-15 09:23:05 +0000

    Associating new topics with the current user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -30,19 +30,21 @@ def topic(request, topic_id):
     
 @login_required
 def new_topic(request):
-	"""Add a new topic"""
-	if request.method != 'POST':
-		#No data submitted; create a blank form
-		form = TopicForm()
-	else:
-		#POST data submitted: process data
-		form = TopicForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect(reverse('learning_logs:topics'))
-	context = {'form': form}
-	return render(request, 'learning_logs/new_topic.html', context)
-		
+    """Add a new topic"""
+    if request.method != 'POST':
+        #No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        #POST data submitted: process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+        
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
